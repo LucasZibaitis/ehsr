245aa0205d86c80612631b75496eac6b3b3959c2
LucasZibaitis <zibaitislucas@gmail.com>
2024-03-20 18:29:18 -0300

    arrancamos el front

---
+++ Front/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center py-10 text-6xl">
+      <div>
+        <h1>CONCEPTIONS</h1>
+      </div>
+    </main>
+  );
+}
